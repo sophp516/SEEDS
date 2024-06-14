02afc3e60731f99a435b3f352b959237a48a1c4a
Sophie Park <sophie.park@dali.dartmouth.edu>
2024-06-14 16:34:01 +0900

    navbar added

--- frontend/App.js
+++ frontend/App.js
@@ -1,13 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native';
+import Home from './pages/Home.jsx';
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>OUR SEEDS PROJECT</Text>
-      <Text>food</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={Home} options={{ headerShown: false }} /> 
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
