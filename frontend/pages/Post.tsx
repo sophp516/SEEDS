95c0fb7f4e9fdecc59b420f08b389bad07ff03f3
Joyce Zou <jzou771@gmail.com>
2024-06-27 10:58:43 -0400

    save post

--- frontend/pages/Post.tsx
+++ frontend/pages/Post.tsx
@@ -1,9 +1,12 @@
 import {useState} from 'react';
 import React from 'react';
-import { StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
-import Navbar from '../components/Navbar.jsx';
+import { Image, Platform, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
 import { useNavigation, NavigationProp, ParamListBase } from '@react-navigation/native';
 import Slider from '@react-native-community/slider';
+import * as ImagePicker from 'expo-image-picker';
+import { db } from '../services/firestore';
+import { collection, addDoc} from 'firebase/firestore';
+// import storage from '@react-native-firebase/storage';
 
 interface newPost{
     image: File;
@@ -15,9 +18,9 @@ interface newReview {
     price: number| null;
     taste: number;
     health: number;
-    image: File;
+    image: string | null;
     tags:string[] | null,
-    comment: string,
+    comment: string|null,
 }
 
 // when click send, passes a object to the firebase function
@@ -32,22 +35,93 @@ const Post = () => {
         price: null,
         taste: 0,
         health: 0,
-        image: {} as File,
+        image: '',
         tags: [],
         comment: '',
     });
+
     const handleCreatePost = () => {
 
     }
-    const handleCreateReview = () =>{
-        // call firebase function
-        navigation.goBack();
+
+    const handleCreateReview = async () =>{
+        try{
+            let finalReview = {...review};
+            console.log("Review added to Firestore with ID:");
+            // ERROR: Image upload doesn't work but else wise it does update on the database
+            // if (review.image){
+            //     const response = await fetch(review.image);
+            //     const blob = await response.blob();
+            //     const imgName = "img-" + new Date().getTime();
+            //     const storageRef = storage().ref(`reviews/${imgName}.jpg`);
+            //     const snapshop = await storageRef.put(blob);
+            //     const imageUrl = await storage().ref(`reviews/${imgName}.jpg`).getDownloadURL();
+            //     finalReview.image = imageUrl;
+            //     console.log("Review added to Firestore with ID:");
+            // }else{  
+            //     delete finalReview.image;
+            // }
+
+            const reviewRef = await addDoc(collection(db, 'reviews'), finalReview);
+            console.log("Review added to Firestore with ID:", reviewRef.id);
+            setReview({ foodName: '',
+                location: '',
+                price: null,
+                taste: 0,
+                health: 0,
+                image: '',
+                tags: [],
+                comment: '',}); // reset the review
+            navigation.goBack();
+        }catch{
+            console.error("Error adding review to Firestore");
+            alert("Error adding review to Firestore");
+        }
     }
+
+    /******* FUNCTIONS FOR UPLOAD IMAGES ******/
+    // Read more about documentation here: https://docs.expo.dev/versions/latest/sdk/imagepicker/ 
+    const getPermission = async() =>{
+        if (Platform.OS === 'ios'){
+            const {status} = await ImagePicker.requestMediaLibraryPermissionsAsync();
+            if (status !== 'granted'){
+                alert('Please grant access to library to upload photos')
+                return false;
+            }
+        }
+        return true;
+    }
+    const selectImage = async() => {
+        const permissionStatus = await getPermission();
+        if (!permissionStatus) return; 
+
+        let result = await ImagePicker.launchImageLibraryAsync({
+            mediaTypes: ImagePicker.MediaTypeOptions.All,
+            allowsEditing: true,
+            aspect:[4,3],
+            quality:1, // we can edit later for more
+        })
+        // console.log("image:", result);        
+        if (!result.canceled){ // if image is selected, then save the latest upload
+            setReview(prevReview => ({...prevReview, image: result.assets[0].uri}))
+        }
+    }
+
+
+  
     /* function handleExit(): Brings user back to the last visited page
     * Also resets the data stored in the use state
      */
     const handleExit = () => {
         navigation.goBack();
+        setReview({ foodName: '',
+            location: '',
+            price: null,
+            taste: 0,
+            health: 0,
+            image: '',
+            tags: [],
+            comment: '',});
     }
     /* Later we can do food reccomendation from API if time */
     const handleChangeFoodName = (text: string) => {
@@ -69,6 +143,8 @@ const Post = () => {
             <TouchableOpacity onPress={handleExit}>
                 <Text>Exit</Text>
              </TouchableOpacity>
+
+
             <View style={styles.toggleContainer}>
                 <TouchableOpacity onPress={()=>setToggle(true)} style={styles.toggleButton}>
                     <Text>Post</Text>
@@ -83,20 +159,27 @@ const Post = () => {
                 </View>
                 : 
                 <View>
+                    
+                    <TouchableOpacity onPress={selectImage}>
+                        <Image source={require('../assets/camera.png')} style={styles.cameraIcon} />
+                    </TouchableOpacity>
                      <Text> Food Name </Text>
                         <TextInput 
+                            style={styles.textbox}
                             value={review.foodName}
                             onChangeText={handleChangeFoodName}
                             placeholder='ex. Apple'
                         />
                     <Text> Location </Text>
                          <TextInput 
+                            style={styles.textbox}
                             value={review.location}
                             onChangeText={handleChangeLocation}
                             placeholder='ex. Collis Cafe'
                         />
                     <Text> Price </Text>
                         <TextInput 
+                            style={styles.textbox}
                             value={review.price ? review.price.toString() :  null}
                             keyboardType='decimal-pad'
                             onChangeText={(text)=> setReview(prevReview => ({...prevReview, price: parseFloat(text) || -1 }))}
@@ -129,6 +212,7 @@ const Post = () => {
                      
                      <Text> Add Tags</Text>
                      <TextInput 
+                            style={styles.textbox}
                             value={tag}
                             onChangeText={(tag)=> setTag(tag)}
                             placeholder='enter a tag'
@@ -145,6 +229,7 @@ const Post = () => {
         </View>
     )
 }
+
 
 const styles = StyleSheet.create({
     container: {
@@ -167,7 +252,16 @@ const styles = StyleSheet.create({
     },
     textbox: {
         borderColor: 'black',
+        backgroundColor: '#D9D9D9',
+        width: 300,
+        height: 30,
+        borderRadius: 10,
+        padding: 10,
     },
+    cameraIcon:{
+        width: '20%',
+        height: '20%',
+    }
 });
 
 export default Post;
--- frontend/services/firestore.js
+++ frontend/services/firestore.js
@@ -13,4 +13,5 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
+export const db = getFirestore(app);
+
