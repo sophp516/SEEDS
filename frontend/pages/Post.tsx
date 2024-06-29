1c9087e36934317033ae336a2ad70fb2d99578d3
Kathy Chen <Kathychen331@outlook.com>
2024-06-29 12:08:22 -0400

    implemented search bar

--- frontend/pages/Post.tsx
+++ frontend/pages/Post.tsx
@@ -1,29 +1,44 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Navbar from '../components/Navbar.jsx';
+import SearchBar from '../components/Searchbar.tsx';
 
 type PostProps = () => {}
 
 const Post = () => {
     return (
+
         <View style={styles.container}>
-            <Text style={styles.text}>Post</Text>
-            {/* <Navbar /> */}
+          <View style={styles.containerTop}>
+            <SearchBar />
+          </View>
+          
+          <Text style={styles.text}>Post</Text>
+          <Navbar /> 
         </View>
+
     )
 }
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        justifyContent: 'center',
+        justifyContent: 'flex-start',
         alignItems: 'center',
-        backgroundColor: 'white', // Set background color if necessary
+        backgroundColor: 'white', 
+        
+    },
+    containerTop: {
+      padding: 20,
+      height: 200,
+      justifyContent: 'center',
+      alignItems: 'center',
+      backgroundColor: 'white', 
     },
     text: {
-        fontSize: 24, // Increase font size for prominence
-        fontWeight: 'bold', // Add bold style for prominence
-        marginBottom: 20, // Add margin bottom to separate from Navbar
+        fontSize: 24, 
+        fontWeight: 'bold', 
+        marginBottom: 20, 
     },
 });
 
